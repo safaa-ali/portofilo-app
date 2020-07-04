f285130e2d03c6671d20d6499039b1da5b6c9f0d
safa <12mlon34@gmail.com>
2020-07-04 02:51:49 +0200

    fix some bugs in service section

--- js/main.js
+++ js/main.js
@@ -9,20 +9,38 @@
 
 
 
- $(window).scroll(function(){
-    var sc =  $(window).scrollTop()
-     if(sc>100){
-      $(".navbar").addClass("navbar-scroll")
-     console.log("welcome back")
+//  $(window).scroll(function(){
+//     var sc =  $(window).scrollTop()
+//      if(sc>100){
+//       $(".navbar").addClass("navbar-scroll")
+//      console.log("welcome back")
  
-     }  
-     else {
-              $(".navbar").removeClass("navbar-scroll");
-           }
-   })
+//      }  
+//      else {
+//               $(".navbar").removeClass("navbar-scroll");
+//            }
+//    })
  
 
-   
+   /* 2. sticky And Scroll UP */
+   $(window).on('scroll', function () {
+    var scroll = $(window).scrollTop();
+    if (scroll < 400) {
+      $(".header-sticky").removeClass("sticky-bar");
+      // $('#back-top').fadeOut(500);
+    } else {
+      $(".header-sticky").addClass("sticky-bar");
+      // $('#back-top').fadeIn(500);
+    }
+  });
+// Scroll Up
+  // $('#back-top a').on("click", function () {
+  //   $('body,html').animate({
+  //     scrollTop: 0
+  //   }, 800);
+  //   return false;
+  // });
+
 
 
 
